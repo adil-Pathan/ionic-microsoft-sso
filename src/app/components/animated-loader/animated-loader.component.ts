74bc2d93d1d8732342a688c66371f2c3a2a73e63
Adil salimkhan Pathan <31478648+adil-Pathan@users.noreply.github.com>
2026-05-16 00:50:43 +0530

    Add AnimatedLoaderComponent TypeScript file for custom loader

---
+++ src/app/components/animated-loader/animated-loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-loader',
+  templateUrl: './animated-loader.component.html',
+  styleUrls: ['./animated-loader.component.scss'],
+})
+export class AnimatedLoaderComponent {}
